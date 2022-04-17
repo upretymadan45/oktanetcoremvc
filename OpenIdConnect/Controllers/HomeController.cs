a8d16b62176bf7686ca63e96f3f4983f527f4d70
Madan <upretymadan45@gmail.com>
2022-04-17 15:33:17 +0545

    project upload

---
+++ OpenIdConnect/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenIdConnect.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles ="Everybone")]
+        public IActionResult Index()
+        {
+            var user = HttpContext.User.Identity.Name;
+            return View();
+        }
+    }
+}
